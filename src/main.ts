81299b3efbe2a459f46d1816936e1cdcf1f74b54
senpp <phuongthephung@gmail.com>
2022-01-13 15:36:09 +0700

    feat: update deps, added helmet, fix some wrong logic

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,27 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { MainModule } from './main.module'
 
+// Security
+import helmet from 'helmet'
+
 async function bootstrap() {
   const app = await NestFactory.create(MainModule)
 
+  app.setGlobalPrefix('v1')
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
   // Enable CORS by whitelist origin
   app.enableCors({
-    origin: process.env.CORS_WHITELIST_ORIGINS,
+    origin: (origin, callback) => {
+      if (process.env.CORS_WHITELIST_ORIGINS.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(null, false)
+      }
+    },
   })
+
+  app.use(helmet())
 
   await app.listen(process.env.PORT || 5001)
 }
